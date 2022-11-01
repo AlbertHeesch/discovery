c963f729786c2778440de65914f754b95ad02e17
Albert Heesch <oddajgrzanki@gmail.com>
2022-11-01 01:51:26 +0100

    First commit

---
+++ src/test/java/com/dent/dentclinicapp/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dent.dentclinicapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
